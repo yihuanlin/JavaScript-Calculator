8f14102d08adca7fdbe859c8ff5af8d415b28a36
Yi, Huanlin <yhl@yhl.ac.cn>
2022-04-19 22:23:05 +0100

    onsubmit = search with DuckDuckGo

--- Moodle.user.js
+++ Moodle.user.js
@@ -12,23 +12,27 @@
 
 (function() {
     'use strict';
-    var div = document.createElement("div");
-    div.style.cssText = "all:initial;transparent;box-shadow:0 0 18px rgba(70,70,40,0.255);background-color:rgba(255,255,255,0.8);border-radius:50px;position:fixed;bottom:30px;left:40px;height:30px;z-index:9999;";
-    div.setAttribute("class", "fromShortcuts");
+    var form = document.createElement("form");
+    form.style.cssText = "all:initial;transparent;box-shadow:0 0 18px rgba(70,70,40,0.255);background-color:rgba(255,255,255,0.8);border-radius:50px;position:fixed;bottom:30px;left:40px;height:30px;z-index:9999;";
+    form.setAttribute("class", "fromShortcuts");
     var input = document.createElement("input");
     input.style.cssText = "all:initial;background-color: transparent;height:30px;margin:0 10px;border:none;font-size:18px;";
     input.setAttribute("class", "fromShortcuts");
-    input.setAttribute('size', 1);
+    input.setAttribute('size', 3);
     input.setAttribute("spellcheck", "false");
     var list = document.createElement("div");
     list.style.cssText = "all:initial;transparent;box-shadow:0 0 18px rgba(70,70,40,0.255);background-color:rgba(255,255,255,0.8);border-radius:20px;position:fixed;bottom:70px;left:40px;height:90px;z-index:9999;display:none;";
     list.setAttribute("class", "fromShortcuts");
-    document.body.appendChild(div);
-    div.appendChild(input);
+    document.body.appendChild(form);
+    form.appendChild(input);
     document.body.appendChild(list);
+    form.onsubmit = function() {
+        window.open("https://duckduckgo.com/?q=" + input.value + "&ia=web", '_blank');
+        return false
+    }
     input.addEventListener('input', function() {
         try {
-            input.setAttribute('size', Math.max(input.value.length, 1));
+            input.setAttribute('size', Math.max(input.value.length, 3));
             let x = eval(input.value.replace("log", "Math.log10").replace("lg", "Math.log10").replace("ln", "Math.log").replace("sqrt", "Math.sqrt").replace("^", "**").replace("^", "**").replace("*", "*").replace("x", "*").replace("e*p", "Math.exp").replace("NA", "6.022e23").replace("R", "8.314").replace("F", "96485").replace("C", "2.998e8").replace("H", "6.626e-34").replace("K", "273.15"));
             let a = x.toExponential().split("e");
             let b = Math.round(a[0] * 1000) / 1000;
